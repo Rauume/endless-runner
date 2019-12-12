7a08133cbade0afab0975e1bd1b49cfcdd69f607
Cam Schriever <cammo8778@gmail.com>
2019-12-12 12:15:50 +1030

    Additions to character controller. Added Gravity flipping mechanic & camera follow script.

--- EndlessRunner/Assets/Scripts/PlayerCharacter.cs
+++ EndlessRunner/Assets/Scripts/PlayerCharacter.cs
@@ -8,16 +8,21 @@ public class PlayerCharacter : MonoBehaviour
 	protected CharacterController m_characterController;
 
 	public LayerMask floorMask;
-	protected bool jumping;
-	public Vector3 appliedVelocity = Vector3.zero;
+	public Vector3 persistentVelocity = Vector3.zero;
+	public Vector3 localGravity = Vector3.zero;
 
 	public float jumpHeight = 5f;
+	public float forwardVelocity = 1f;
+	public float maxFallVelocity;
+	public float gravityMultiplier;
 
 	protected RaycastHit groundHit;
 	public float groundDetectionLength;
 
+	public bool GravityIsDown = true;
+
 	// Start is called before the first frame update
-	void Start()
+	private void Awake()
 	{
 		m_animator = GetComponent<Animator>();
 		m_characterController = GetComponent<CharacterController>();
@@ -26,42 +31,53 @@ public class PlayerCharacter : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		appliedVelocity += Physics.gravity / 2 * Time.deltaTime;
+		if (GravityIsDown)
+			localGravity = Physics.gravity / 2 * gravityMultiplier;
+		else
+			localGravity = -Physics.gravity / 2 * gravityMultiplier;
 
+		persistentVelocity += localGravity * Time.deltaTime;
+		persistentVelocity.y = Mathf.Clamp(persistentVelocity.y, maxFallVelocity, -maxFallVelocity);
 
-		if (Input.GetKeyDown(KeyCode.Space))
-		{
-			Input_Jump();
-		}
 
 		if (IsGrounded()) //if is on the ground
 		{
 			m_animator.SetBool("Jumping", false); //set the animation state
+			persistentVelocity.y = groundHit.point.y - transform.position.y; //set velocity to the ground.
 		}
 
-		m_characterController.Move(appliedVelocity * Time.deltaTime);
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Input_Jump();
+			Input_SwitchDirections();
+		}
 	}
 
-
-	private void OnAnimatorMove()
+	//control the root motion via script (Probs don't need, nothing is animation controlled anymore)
+	private void OnAnimatorMove() 
 	{
-		Vector3 outcomeVelocity = (appliedVelocity + m_animator.velocity);
-		outcomeVelocity.z = 0;
+		Vector3 outcomeVelocity = (persistentVelocity);
 
+		//keep the animation from deviating from the running line
+		outcomeVelocity.z = 0; 
+		//constant fowards velocity
+		outcomeVelocity.x = forwardVelocity; 
 
-		m_characterController.Move(outcomeVelocity * Time.deltaTime);
+		//apply the velocity to the character.
+		m_characterController.Move(outcomeVelocity * Time.deltaTime); 
 	}
 
 	public bool IsGrounded()
 	{
-		Ray ray = new Ray(transform.position, Vector3.down);
-		bool value = Physics.Raycast(transform.position + 0.5f * Vector3.up, Vector3.down, out groundHit, groundDetectionLength);
+		//raycast downwards to detect the edges of the screen.
+		Ray ray = new Ray(transform.position, -transform.up);
+		bool value = Physics.Raycast(transform.position + 1f * transform.up, -transform.up, out groundHit, groundDetectionLength);
 
-		//if moving upwards.
-		if (appliedVelocity.y > 0)
+		//if within distance of the ground
+		if (Vector3.Distance(groundHit.point, transform.position) < 0.1)
+			return true;
+		else //else, still in air.
 			return false;
-
-		return value;
 	}
 
 
@@ -69,10 +85,23 @@ public class PlayerCharacter : MonoBehaviour
 	{
 		if (IsGrounded())
 		{
-			appliedVelocity.y = Mathf.Sqrt(-2.0f * Physics.gravity.y * jumpHeight);
-
+			//Jump!
 			m_animator.SetBool("Jumping", true);
-			jumping = true;
+		}
+	}
+
+	//change the gravity depending on the previous direction
+	public void Input_SwitchDirections()
+	{
+		if (GravityIsDown)
+		{
+			GravityIsDown = false;
+			transform.eulerAngles += Vector3.forward * 180;
+		}
+		else
+		{
+			GravityIsDown = true;
+			transform.eulerAngles += Vector3.forward * -180;
 		}
 	}
 
